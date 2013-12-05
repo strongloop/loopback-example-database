d93e6930bde694628950c7ba39e4ff3f7f97840a
Raymond Feng <raymond@strongloop.com>
2013-12-04 19:50:13 -0800

    Add onwer option to avoid confusions over other dbs

--- discover.js
+++ discover.js
@@ -4,7 +4,7 @@ dataSource.discoverSchema('account', {owner: 'demo'}, function (err, schema) {
     console.log(JSON.stringify(schema, null, '  '));
 });
 
-dataSource.discoverAndBuildModels('account', {}, function (err, models) {
+dataSource.discoverAndBuildModels('account', {owner: 'demo'}, function (err, models) {
     models.Account.find(function (err, act) {
         if (err) {
             console.error(err);
