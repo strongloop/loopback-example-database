6978aa2f0b2a7bb07b3aca00ada8523ff346a74e
Simon Ho <simon@strongloop.com>
2014-09-02 11:35:06 -0700

    Update discover.js to use Oracle settings

--- server/discover.js
+++ server/discover.js
@@ -1,12 +1,12 @@
 var server = require('./server');
 var dataSource = server.dataSources.accountDB;
 
-dataSource.discoverSchema('account', { owner: 'demo' }, function(er, schema) {
+dataSource.discoverSchema('ACCOUNT', { owner: 'DEMO' }, function(er, schema) {
   if (er) throw er;
   console.log(JSON.stringify(schema, null, '  '));
 });
 
-dataSource.discoverAndBuildModels('account', { owner: 'demo' },
+dataSource.discoverAndBuildModels('ACCOUNT', { owner: 'DEMO' },
     function(er, models) {
   if (er) throw er;
   models.Account.find(function(er, accounts) {
