ada5e000566c9fbc1f40f38f1387b5bb20735ed3
Ismail <ismail-s@users.noreply.github.com>
2015-10-30 14:03:34 +0000

    Update lastModifedAt field when model is updated
    Fixes #24

--- common/models/account.js
+++ common/models/account.js
@@ -1,3 +1,11 @@
 module.exports = function(Account) {
-
+Account.observe('before save', function (ctx, next) {
+    if (ctx.instance) {
+        ctx.instance.lastModifiedAt = new Date();
+    }
+    else {
+        ctx.data.lastModifiedAt = new Date();
+    }
+    next();
+});
 };
