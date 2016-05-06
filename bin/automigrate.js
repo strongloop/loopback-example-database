3a72159cbc6260d294cc34588d6b2e1c4a13bc76
Ryan Graham <r.m.graham@gmail.com>
2016-05-05 21:34:04 -0700

    update copyright notices and license

--- bin/automigrate.js
+++ bin/automigrate.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2015,2016. All Rights Reserved.
+// Node module: loopback-example-database
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 var path = require('path');
 
 var app = require(path.resolve(__dirname, '../server/server'));
